480f3bc12ad9df734d4d917d86ab688cadd6a874
Yamaguchi <ymgc2003rm@me.com>
2015-05-29 08:59:50 +0000

    Finish 7.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  before_save { self.email = email.downcase! }
+  before_save { email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
